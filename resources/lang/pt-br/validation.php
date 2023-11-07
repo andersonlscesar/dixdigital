989969f08b428ae5dbf8017bb74c72bd67056d88
andersonlscesar <andersonlscesar@gmail.com>
2023-11-06 22:02:25 -0300

    traduzindo erros de validações

---
+++ resources/lang/pt-br/validation.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'custom' => [
+        'name' => [
+            'required' => 'O campo "Nome" é obrigatório'
+        ],
+        'email' => [
+            'required' => 'O campo "E-mail" é obrigatório'
+        ],
+        'password' => [
+            'required' => 'O campo "Senha" é obrigatório'
+        ]
+    ]
+];
